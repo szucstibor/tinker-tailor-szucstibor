ece5118f55efaea9dafd7a430e8086b6c0edfade
Szűcs Tibor <job.szucstibor@gmail.com>
2017-11-17 14:24:51 +0100

    Lucky numnber 6 bug has been fixed

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,29 @@ public class Main {
 
     public static void main(String[] args) {
         List players = new ArrayList();
-        for (int playerNumber = 1; playerNumber < 6; playerNumber++) {
+        for (int playerNumber = 1; playerNumber < 1024; playerNumber++) {
             players.add(playerNumber);
         }
-        game(players);
+        game(players ,3);
         System.out.println("The winner is player "+players.get(0));
 
 
     }
 
-    public static List game(List players) {
+    public static List game(List players, int knockoutNumber) {
         int startingPoint = 0;
         while (players.size() != 1) {
             ArrayList tinkerTailor = new ArrayList() {};
-            while (tinkerTailor.size() < 8 + startingPoint) {
+            while (tinkerTailor.size() < knockoutNumber + startingPoint) {
                 for (int player = 0; player < players.size(); player++) {
                     tinkerTailor.add(players.get(player));
                 }
             }
             for (int player = 0; player < players.size(); player++) {
-                if (players.get(player) == tinkerTailor.get(7 + startingPoint)){
+                if (players.get(player) == tinkerTailor.get(knockoutNumber + startingPoint - 1)){
                     System.out.println("Player "+players.get(player)+" is knocked out");
                     players.remove(player);
-                    startingPoint = 7 - player;
+                    startingPoint = player;
                     break;
                 }
             }
